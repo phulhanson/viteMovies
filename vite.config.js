31d46da04dc0bd58d660f5e40452789de7c9b124
Paul Hanson <paulhanson099@gmail.com>
2026-04-28 00:30:06 +0100

    Fix for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === 'production' ? '/viteMovies/' : '/',
-    plugins: [react()],
-  }
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
 })
